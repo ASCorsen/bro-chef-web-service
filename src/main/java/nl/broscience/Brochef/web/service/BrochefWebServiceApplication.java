5f8c23680f6c2b226fbcc8b871e5fede66686972
ASCorsen <89386377+ASCorsen@users.noreply.github.com>
2023-01-21 15:41:32 +0100

    Untracked some files, added DB and propertie resources

---
+++ src/main/java/nl/broscience/Brochef/web/service/BrochefWebServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.broscience.Brochef.web.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrochefWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrochefWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/broscience/Brochef/web/service/BrochefWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.broscience.Brochef.web.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrochefWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
